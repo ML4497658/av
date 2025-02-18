39598680f7d936eda36ce5486e45db74189b05fe
Dujunmeng <563778000@qq.com>
2025-02-18 14:26:37 +0800

    Create av.js

---
+++ av.js
@@ -0,0 +1,63 @@
+{
+	"desc": "此链接为 MusicFree 插件，插件开发及使用方式参考 https://musicfree.upup.fun",
+	"plugins": [{
+		"name": "小枸音乐（酷狗）",
+		"url": "https://raw.niuma666bet.buzz/Huibq/keep-alive/master/Music_Free/xiaogou.js",
+		"version": "0.0.2"
+	},
+	{
+		"name": "小秋音乐（QQ）",
+		"url": "https://raw.niuma666bet.buzz/Huibq/keep-alive/master/Music_Free/xiaoqiu.js",
+		"version": "0.0.0"
+	},
+	{
+		"name": "小蜗音乐（酷我）",
+		"url": "https://raw.niuma666bet.buzz/Huibq/keep-alive/master/Music_Free/xiaowo.js",
+		"version": "0.2.2"
+	},
+	{
+		"name": "小蜜音乐（咪咕）",
+		"url": "https://raw.niuma666bet.buzz/Huibq/keep-alive/master/Music_Free/xiaomi.js",
+		"version": "0.0.2"
+	},
+	{
+		"name": "小芸音乐（网易云）",
+		"url": "https://raw.niuma666bet.buzz/Huibq/keep-alive/master/Music_Free/xiaoyun.js",
+		"version": "0.0.0"
+	},
+	{
+		"name": "QQ音乐",
+		"url": "http://adad23u.appinstall.life/dist/qq/index.js",
+		"version": "0.0.0"
+	},
+	{
+		"name": "网易云音乐",
+		"url": "https://gitee.com/raycn120/musicfree/raw/master/netease.js",
+		"version": "0.0.0"
+	},
+	{
+		"name": "猫耳FM",
+		"url": "https://gitee.com/maotoumao/MusicFreePlugins/raw/v0.1/dist/maoerfm/index.js",
+		"version": "0.1.4"
+	},
+	{
+		"name": "Bilibili",
+		"url": "http://adad23u.appinstall.life/dist/bilibili/index.js",
+		"version": "0.0.2"
+	},
+	{
+		"name": "酷我音乐",
+		"url": "http://adad23u.appinstall.life/dist/kuwo/index.js",
+		"version": "0.0.0"
+	},
+	{
+		"name": "音悦台",
+		"url": "https://gitee.com/maotoumao/MusicFreePlugins/raw/v0.1/dist/yinyuetai/index.js",
+		"version": "0.0.1"
+	},
+	{
+		"name": "咪咕音乐",
+		"url": "http://adad23u.appinstall.life/dist/migu/index.js",
+		"version": "0.0.1"
+	}]
+}
